2d2feefd2997026a9edf08705222dc6f4c473659
Li Dai <lisadai@umich.edu>
2025-05-23 01:25:01 -0400

    added session cookie functionality, secure hashed and salted pwd validation

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from webdiary import diary_bp
 from art import art_bp
 
 app = Flask(__name__)
+app.secret_key = 'my-secret-key'
 
 # Register blueprints
 app.register_blueprint(diary_bp)
--- webdiary.py
+++ webdiary.py
@@ -6,26 +6,55 @@ URLs include:
 """
 import sqlite3
 from datetime import datetime
+import uuid
+import hashlib
 import flask
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, session, request, flash, abort
 
 diary_bp = Blueprint('diary_bp', __name__)
 
 @diary_bp.route('/', methods=['GET'])
 def index():
     """Return Index."""
-    return flask.render_template('index.html')
+    return render_template('index.html')
 
 @diary_bp.route('/diary/', methods=['GET'])
 def diary():
     """Render Diary."""
-
+    # not authorized to view diary
+    if "authenticated" not in session:
+        return render_template('diary.html', posts=None, authenticated=False)
+    
     conn = sqlite3.connect('var/webdiary.sqlite3')
     # returns post as dictionaries instead of tuples
     conn.row_factory = sqlite3.Row
     posts = conn.execute('SELECT * FROM posts ORDER BY created DESC').fetchall()
     conn.close()
-    return render_template('diary.html', posts=posts)
+    return render_template('diary.html', posts=posts, authenticated=True)
+
+@diary_bp.route('/validatepwd/', methods=['POST'])
+def validate():
+    """Validate Password to View Diary Posts."""
+
+    stored_pwd = 'sha512$210b91ab17c1490b8d66858497df60c0$1a9d624a7cf1ed3554969dd68c987e14d7fb790ddbe4c331aa032486fc0264487335f7ea942ed15bf5ec69e119f491be67fc41973f2bb3fc737b29092d22aee4'
+    algorithm, salt, stored_hash = stored_pwd.split('$')
+
+    input_pwd = request.form['pass']
+
+    hash_obj = hashlib.new(algorithm)
+    salted_pwd = salt + input_pwd
+    # encodes salted pwd string into UTF-8 bytes, as hashing functions require bytes as input
+    hash_obj.update(salted_pwd.encode('utf-8'))
+    # computes the final SHA-512 hash of the salted password, returns as hexadecimal str
+    input_hash = hash_obj.hexdigest()
+
+    if input_hash == stored_hash:
+        session['authenticated'] = True
+        return redirect(url_for('diary_bp.diary'))
+    else:
+        flash('Incorrect Password')
+        return redirect(url_for('diary_bp.diary'))
+    
 
 @diary_bp.route('/submit/', methods=['POST'])
 def handle_posts():
@@ -34,12 +63,12 @@ def handle_posts():
     # Connect to database
     conn = sqlite3.connect('var/webdiary.sqlite3')
 
-    title = flask.request.form['title']
-    text = flask.request.form['text']
+    title = request.form['title']
+    text = request.form['text']
     created = datetime.now().replace(microsecond=0)
 
     if not text:
-        flask.abort(400)  # ERROR: text is empty
+        abort(400)  # ERROR: text is empty
     else:
         # insert new post
         conn.execute("INSERT INTO posts (title, text, created)"
@@ -47,12 +76,12 @@ def handle_posts():
                     (title, text, created))
         # need to commit changes after executing SQL command
         conn.commit()
-    return flask.redirect('/diary/')
+    return redirect('/diary/')
 
 @diary_bp.route('/new-post/', methods=['GET'])
 def newpost():
     """Return New Diary Post Form."""
-    return flask.render_template('new-post.html')
+    return render_template('new-post.html')
 
 @diary_bp.route('/diarydelete/', methods=['POST'])
 def diarydelete():
@@ -61,8 +90,8 @@ def diarydelete():
     conn = sqlite3.connect('var/webdiary.sqlite3')
     conn.row_factory = sqlite3.Row
 
-    postid = flask.request.form['postid']
+    postid = request.form['postid']
     conn.execute("DELETE FROM posts WHERE postid=?",
                  (postid,))
     conn.commit()
-    return flask.redirect('/diary/')
+    return redirect('/diary/')
